195f796e68be2697086a879c383b5d3f82cd8e27
Christian Tejeda <christiantejeda@proton.me>
2026-02-05 12:16:07 -0300

    Solved Captain's Log

---
+++ captains-log/src/main/java/CaptainsLog.java
@@ -0,0 +1,26 @@
+import java.util.Random;
+
+class CaptainsLog {
+
+    private static final char[] PLANET_CLASSES = new char[]{'D', 'H', 'J', 'K', 'L', 'M', 'N', 'R', 'T', 'Y'};
+
+    private Random random;
+
+    CaptainsLog(Random random) {
+        this.random = random;
+    }
+
+    char randomPlanetClass() {
+        int index = random.nextInt(PLANET_CLASSES.length);
+        return PLANET_CLASSES[index];
+    }
+
+    String randomShipRegistryNumber() {
+        int shipNumber = random.nextInt(1000, 10000);
+        return "NCC-" + shipNumber;
+    }
+
+    double randomStardate() {
+        return random.nextDouble(41000, 42000);
+    }
+}

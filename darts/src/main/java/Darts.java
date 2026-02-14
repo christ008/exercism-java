2aa47bd565cda288f6b7e21134cb4af2d28b9324
Christian Tejeda <christiantejeda@proton.me>
2026-02-13 21:44:07 -0300

    Added Darts solution

---
+++ darts/src/main/java/Darts.java
@@ -0,0 +1,9 @@
+class Darts {
+    int score(double xOfDart, double yOfDart) {
+        double distance = Math.sqrt(yOfDart * yOfDart + xOfDart * xOfDart);
+        if (distance >= 0 && distance <= 1) return 10;
+        if (distance > 1 && distance <= 5) return 5;
+        if (distance > 5 && distance <= 10) return 1;
+        return 0;
+    }
+}
